d7443b31524aea363f608f9ccbcd3cc575a4a758
Vinicius Pinheiro <vinicius.pinheiro@wisereducacao.com>
2021-04-11 18:45:06 -0300

    feat: fixed function add convetional method func

--- main.c
+++ main.c
@@ -2,55 +2,76 @@
 
 #define CUSTO_FIXO 6
 #define PRECO_KG 18
-#define LARGURA_FAIXA 100
-#define DESCONTO_FAIXA 0.1
-#define DESCONTO_MAX 0.5
+#define LARGURA_FAIXA 80
+#define DESCONTO_FAIXA 0.05
+#define DESCONTO_MAX 0.6
 
 float calculaValorDevido(float peso, float custo_fixo, float preco_kg, float largura_faixa, float desconto_faixa, float desconto_max)
 {
-  float valorFinal = custo_fixo;
-  float soma;
-  int contador = 1;
-  float numFaixa;
+  float parcelaPeso = largura_faixa, soma = 0;
+  // Inicialmente usa o próprio peso:
+  float parcelaSemDesconto = (preco_kg * peso) / 1000;
+  int i = 1, jaCalculou = 0;
 
-  if (peso > 100)
+  if (peso > largura_faixa)
   {
-    peso = peso - 100;
-    // Numero de vezes que a largura esta presente dentro do peso do prato:
-    numFaixa = peso / LARGURA_FAIXA;
+    // Retira a primeira faixa, para não aplicar o desconto errado:
+    peso -= largura_faixa;
+    // Usa a largura da faixa, pq o peso é maior:
+    parcelaSemDesconto = (preco_kg * largura_faixa) / 1000;
 
-    if (numFaixa <= 0)
-      numFaixa == 1;
-
-    // O programa vai rodar o número de vezes que a faixa existe dentro do peso do prato:
-    while (numFaixa > 0)
+    while (!jaCalculou)
     {
-      // Caso atinja o desconto máximo,
-      if (((float)contador / 10) == desconto_max)
+      // Ex: largura faixa é 100, e o peso é 130. Entao precisamos pegar esse 30 em vez de 100.
+      if ((LARGURA_FAIXA * i) > peso)
       {
-        soma += ((peso * 18) / 1000) * desconto_max;
+        parcelaPeso = LARGURA_FAIXA - ((LARGURA_FAIXA * i) - peso);
       }
       else
       {
-        // Soma com o próprio multiplicador "i"
-        soma += peso ;
+        parcelaPeso = largura_faixa;
       }
-      contador++;
-      numFaixa--;
+
+      // Preço por quilo atual, com valor máximo o valor da faixa:
+      parcelaPeso = (preco_kg * parcelaPeso) / 1000;
+
+      // Caso tenha atingido o desconto máximo, então não aplica mais o desconto por faixa:
+      if (i * desconto_faixa > desconto_max)
+      {
+        soma += parcelaPeso * (1 - desconto_max);
+      }
+      else
+      {
+        // Soma, já aplicando o desconto:
+        soma += parcelaPeso * (1 - (desconto_faixa * i));
+      }
+
+      i++;
+
+      // Se no próximo ciclo a diferença entre o peso e a faixa atual for maior que a própria faixa,
+      // significa que já foi englobado todo o valor:
+      if (((LARGURA_FAIXA * i) - peso) >= largura_faixa)
+        jaCalculou = 1;
     }
   }
 
-  return valorFinal + ((peso * 18) / 1000);
+  return custo_fixo + parcelaSemDesconto + soma;
+}
+
+float calculaValorDevidoConvencial(float peso, float custo_fixo, float preco_kg)
+{
+  return custo_fixo + (preco_kg * peso / 1000);
 }
 
 int main()
 {
 
-  float peso, precoFinal;
+  float peso, precoFinal, eConvencional;
 
   printf("Olá! Informe o peso do prato do cliente: ");
   scanf("%f", &peso);
 
+  // precoFinal = calculaValorDevidoConvencial(peso, CUSTO_FIXO, PRECO_KG);
   precoFinal = calculaValorDevido(peso, CUSTO_FIXO, PRECO_KG, LARGURA_FAIXA, DESCONTO_FAIXA, DESCONTO_MAX);
 
   printf("O Preco final do prato eh R$%.2f\n", precoFinal);
